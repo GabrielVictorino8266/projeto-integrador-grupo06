424e69e18a480472f9965223c9773edfbece6293
GabrielVictorino8266 <gabriel.victorino2004@gmail.com>
2023-10-23 07:58:11 -0300

    Finalizacao responsividade Css pagina cadastro

--- sprint3/script.js
+++ sprint3/script.js
@@ -6,46 +6,46 @@ fotoPerfilPostagens[0].alt = ''
 fotoPerfilPostagens[1].alt = ''
 fotoPerfilPostagens[2].alt = ''
 
-const nav = document.querySelector(".nav");
-const btnMenu = document.querySelector(".btn-menu");
-const menu = document.querySelector(".menu");
+// const nav = document.querySelector(".nav");
+// const btnMenu = document.querySelector(".btn-menu");
+// const menu = document.querySelector(".menu");
 
-function handleButtonClick(event){
-    if(event.type === "touchstart") event.preventDefault(); // Interrompe o comportamento padrao no mobile.
-    event.stopPropagation();
-    nav.classList.toggle("active");
-    handleClickOutside (menu, () => {
-        nav.classList.remove("active");
-    }) 
-}
+// function handleButtonClick(event){
+//     if(event.type === "touchstart") event.preventDefault(); // Interrompe o comportamento padrao no mobile.
+//     event.stopPropagation();
+//     nav.classList.toggle("active");
+//     handleClickOutside (menu, () => {
+//         nav.classList.remove("active");
+//     }) 
+// }
 
-function handleClickOutside(targetElement, callback){
-    const html = document.documentElement;
-    function handdleHTMLClick(event){
-        if(!targetElement.contains(event.target)){
-            targetElement.removeAttribute("data-target");
-            html.removeEventListener("click", handdleHTMLClick);
-            html.removeEventListener("touchstart", handdleHTMLClick);
-            callback()
-        }
-    }
+// function handleClickOutside(targetElement, callback){
+//     const html = document.documentElement;
+//     function handdleHTMLClick(event){
+//         if(!targetElement.contains(event.target)){
+//             targetElement.removeAttribute("data-target");
+//             html.removeEventListener("click", handdleHTMLClick);
+//             html.removeEventListener("touchstart", handdleHTMLClick);
+//             callback()
+//         }
+//     }
 
-    if (!targetElement.hasAttribute("data-target")){
-        html.addEventListener("click", handdleHTMLClick);
-        html.addEventListener("touchstart", handdleHTMLClick);
-        targetElement.setAttribute("data-target", "");
-    }
-}
+//     if (!targetElement.hasAttribute("data-target")){
+//         html.addEventListener("click", handdleHTMLClick);
+//         html.addEventListener("touchstart", handdleHTMLClick);
+//         targetElement.setAttribute("data-target", "");
+//     }
+// }
 
-function setAria() {
-    const isActive = nac.classList.contains("active");
-    btnMenu.setAttribute("aria-expander", isActive);
-    if (isActive){
-        btnMenu.setAttribute("aria-label", "Fechar Menu");
-    } else {
-        btnMenu.setAttribute("aria-label", "Abrir Menu");
-    }
-}
+// function setAria() {
+//     const isActive = nac.classList.contains("active");
+//     btnMenu.setAttribute("aria-expander", isActive);
+//     if (isActive){
+//         btnMenu.setAttribute("aria-label", "Fechar Menu");
+//     } else {
+//         btnMenu.setAttribute("aria-label", "Abrir Menu");
+//     }
+// }
 
-btnMenu.addEventListener("click", handleButtonClick);
-btnMenu.addEventListener("touchstart", handleButtonClick);
+// btnMenu.addEventListener("click", handleButtonClick);
+// btnMenu.addEventListener("touchstart", handleButtonClick);
