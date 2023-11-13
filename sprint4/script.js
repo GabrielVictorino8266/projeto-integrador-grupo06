5244a435c65e59bb821eb3508250f7bddc211222
GabrielVictorino8266 <gabriel.victorino2004@gmail.com>
2023-11-13 08:22:54 -0300

    Posts-Criados e btn like e unlike adicionados

--- sprint4/script.js
+++ sprint4/script.js
@@ -1,7 +1,43 @@
 // Codigo JS
+let btnClicked = false
 
-const fotoPerfilPostagens = document.querySelectorAll('img')
+const btnLike = document.querySelector(".like-btn")
+const btnUnlike = document.querySelector(".unlike-btn")
 
-fotoPerfilPostagens[0].alt = ''
-fotoPerfilPostagens[1].alt = ''
-fotoPerfilPostagens[2].alt = ''
+
+function like(){
+    if (btnClicked === false){
+        btnLike.innerHTML = `
+        <i class="fa-solid fa-thumbs-up"></i>
+        `;
+        btnClicked = true;
+
+    }
+    else if(btnClicked === true){
+
+        btnLike.innerHTML = `
+        <i class="fa-regular fa-thumbs-up"></i>
+        `;
+        btnClicked = false;
+    }
+}
+
+function unlike(){
+    if (btnClicked === false){
+        btnUnlike.innerHTML = `
+        <i class="fa-solid fa-thumbs-down"></i>
+        `;
+        btnClicked = true;
+
+    }
+    else if(btnClicked === true){
+
+        btnUnlike.innerHTML = `
+        <i class="fa-regular fa-thumbs-down"></i>
+        `;
+        btnClicked = false;
+    }
+}
+
+btnLike.addEventListener(click, like)
+btnUnlike.addEventListener(click, unlike)
