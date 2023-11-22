1f6f018588b8b54f29f73ed591199f75b98e273a
GabrielVictorino8266 <gabriel.victorino2004@gmail.com>
2023-11-22 20:10:53 -0300

    Correcoes gerais

--- sprint5/banco de dados/sprin5.sql
+++ sprint5/banco de dados/sprin5.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE db_sprint5ç
+
 USE db_sprint5;
 
 create table if not exists cargos(
@@ -22,7 +24,7 @@ create table if not exists artigos(
 id int auto_increment primary key not null,
 titulo varchar(70) not null,
 texto text not null,
-data_artigo datetime,
+data_artigo date,
 id_usuario int not null,
 FOREIGN KEY(id_usuario) REFERENCES usuarios(id)
 );
@@ -42,7 +44,7 @@ descricao varchar(250)
 );
 
 /*Associacao entre Imagens e Categoria*/
-create table if not exists artigos_x_categoria(
+create table if not exists artigos_x_categorias(
 id_categoria int not null,
 id_artigo int not null,
 foreign key(id_artigo) references artigos(id),
@@ -50,3 +52,47 @@ foreign key(id_categoria) references categorias(id),
 primary key(id_categoria, id_artigo) /*Uso de uma chave primaria composta*/
 );
 
+
+
+/************Insercao de Valores no Banco************/
+
+INSERT INTO cargos(nome, descricao, permissao) VALUES ("administrador", "acesso total ao sistema.", 1), ("usuario_comum", "Pode visualizar e interagir com a postagem", 2);
+SELECT * FROM CARGOS;
+
+INSERT INTO usuarios(nome, email, cpf, senha, email_confirmado, id_cargo) VALUES ("Pedro", "pedro@gmail.com", "12345678900", "root", true, 1), ("Fabricio", "fabricio@gmail.com", "00987654321", "comum123", false, 2);
+SELECT * FROM USUARIOS;
+
+INSERT INTO artigos(titulo, texto, data_artigo, id_usuario) VALUES 
+("netflix vs nasa", "Agora a nasa ira concorrer contra a netflix, pois criou um sistema de streaming sobre conteudo relacionado a ciencia, etc", "2023-11-12", 1),
+("chatgpt: uma revolucao", "nova revolucao na tecnologia, chatgpt agora consegue ser integrado a outros sistemas de inteligencia artificial.", "2023-11-15", 1);
+SELECT * FROM artigos;
+
+INSERT INTO imagens(src, descricao, id_artigo) VALUES ("./img/nasa-vs-netflix.png", "Foto da Logo da Nasa vs a Netflix", 1), ("./img/chatgpt-revolucao.png", "Foto da Logo do ChatGPT", 2);
+SELECT * FROM imagens;
+
+INSERT INTO categorias(nome, descricao) VALUES ("tech", "conteudo sobre o mundo tech"), ("science", "conteudo sobre ciencia e pesquisas");
+SELECT * FROM categorias;
+
+
+INSERT INTO artigos_x_categorias(id_categoria, id_artigo) VALUES (1,1);
+SELECT * FROM artigos_x_categorias;
+
+
+
+/************Clausulas de Consultas (SELECT)************/
+SELECT u.nome, c.nome FROM usuarios AS u
+INNER JOIN cargos AS c ON u.id_cargo = c.id
+WHERE u.email_confirmado = TRUE;
+    
+SELECT a.titulo, a.texto from artigos AS a
+INNER JOIN artigos_x_categorias AS ac ON a.id = ac.id_artigo
+INNER JOIN categorias AS ca ON ac.id_categoria = ca.id
+WHERE ca.nome = 'tech';
+    
+SELECT artigos.titulo, usuarios.nome FROM artigos
+JOIN usuarios ON artigos.id_usuario = usuarios.id;
+
+SELECT usuarios.nome, COUNT(artigos.id) AS 'Número de Artigos'
+FROM usuarios
+JOIN artigos ON usuarios.id = artigos.id_usuario
+GROUP BY usuarios.nome;
