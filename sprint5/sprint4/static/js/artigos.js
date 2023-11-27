6a0bdc5c9687289b2517c7f740ad50e86fe3c205
GabrielVictorino8266 <gabriel.victorino2004@gmail.com>
2023-11-27 07:27:33 -0300

    Reposicionando pastas

--- sprint5/sprint4/static/js/artigos.js
+++
@@ -1,9 +0,0 @@
-function getArticle() {
-    const artigo = document.getElementById("post");
-    localStorage.setItem("lastArticle", "Oi");
-    artigo.innerText = localStorage.getItem("lastArticle");
-  }
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("testeArt").addEventListener('click', getArticle);
-  });
